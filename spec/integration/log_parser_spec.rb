5698fb8dc20820ee1340773f68ad45aa030f96b3
Martin Chatterton <martin.paul.chatterton@gmail.com>
2022-11-25 17:32:43 +0000

    chore: add more integration tests

--- spec/integration/log_parser_spec.rb
+++ spec/integration/log_parser_spec.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'open3'
 
 RSpec.describe './log_parser' do
-  context 'with valid arguments' do
+  context 'with default ---output-type' do
     it 'gives the correct response' do
       output = `./log_parser spec/support/fixtures/example.log`
 
@@ -13,6 +13,34 @@ RSpec.describe './log_parser' do
           '/help_page/1' => 3,
           '/about' => 2,
           '/home' => 1
+        }
+      }))
+    end
+  end
+
+  context 'with --output-type=page_views' do
+    it 'gives the correct response' do
+      output = `./log_parser --output-type=page_views spec/support/fixtures/example.log`
+
+      expect(output).to eq(JSON.generate({
+        'page_views' => {
+          '/help_page/1' => 3,
+          '/about' => 2,
+          '/home' => 1
+        }
+      }))
+    end
+  end
+
+  context 'with --output-type=unique_page_views' do
+    it 'gives the correct response' do
+      output = `./log_parser --output-type=unique_page_views spec/support/fixtures/example.log`
+
+      expect(output).to eq(JSON.generate({
+        'unique_page_views' => {
+          '/help_page/1' => 2,
+          '/home' => 1,
+          '/about' => 1
         }
       }))
     end
@@ -35,40 +63,6 @@ RSpec.describe './log_parser' do
       expect(stderr).to include(
         'ERROR: "log_parser" was called with arguments "--foo-bar=baz"'
       )
-    end
-  end
-
-  context 'with help flag' do
-    it 'shows the usage instructions' do
-      output = `./log_parser -h`
-
-      expected_output = <<~OUTPUT
-      Command:
-        log_parser
-
-      Usage:
-        log_parser FILE_PATH
-
-      Description:
-        Command line tool for processing page view logs.
-
-        Page view logs must be in the format :-
-          [page_view] [ip_address]
-
-        For example :-
-          /home 184.123.665.067
-          /about/2 444.701.448.104
-          etc
-
-      Arguments:
-        FILE_PATH                         # REQUIRED Path to log file
-
-      Options:
-        --output-type=VALUE, -t VALUE     # Output type: (page_views/unique_page_views), default: "page_views"
-        --help, -h                        # Print this help
-      OUTPUT
-
-      expect(output).to eq(expected_output)
     end
   end
 end
