f354eb232fc56ec80d06f07a1ec50cf58a3c1992
Martin Chatterton <martin.paul.chatterton@gmail.com>
2022-12-02 17:22:38 +0000

    refactor: make logs var consistently named

--- lib/aggregator/page_views.rb
+++ lib/aggregator/page_views.rb
@@ -4,11 +4,11 @@ require 'page_aggregate'
 
 module Aggregator
   class PageViews
-    def call(logs_to_process)
-      logs_grouped_by_page = logs_to_process.group_by(&:page)
+    def call(logs)
+      logs_grouped_by_page = logs.group_by(&:page)
 
-      logs_grouped_by_page.map do |page, logs|
-        PageAggregate.new(page:, count: logs.count)
+      logs_grouped_by_page.map do |page, page_logs|
+        PageAggregate.new(page:, count: page_logs.count)
       end
     end
   end
